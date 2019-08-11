9a763f6168622b619efd6b3868f3bdfb91b5f092
Dpac99 <diogo.a.pacheco@tecnico.ulisboa.pt>
2019-08-11 17:14:39 +0000

    adding tests

--- src/router/meetings_test.go
+++ src/router/meetings_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 	"time"
 
-	//"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/sinfo/deck2/src/models"
 	"github.com/sinfo/deck2/src/mongodb"
@@ -46,10 +46,41 @@ func TestCreateMeeting(t *testing.T){
 	assert.NilError(t, err)
 	assert.Equal(t, res.Code, http.StatusOK)
 
+	Meeting1, err = mongodb.Meetings.GetMeeting(meeting.ID)
+	assert.NilError(t, err)
+
 	json.NewDecoder(res.Body).Decode(&meeting)
-	//assert.Equal(t, meeting.Begin, *Meeting1Data.Begin)
-	//assert.Equal(t, meeting.End, *Meeting1Data.End)
-	assert.Equal(t, meeting.Place, *Meeting1Data.Place)
+	assert.Equal(t, meeting.ID, Meeting1.ID)
+	assert.Equal(t, meeting.Place, Meeting1.Place)
+}
+
+func TestCreateMeetingWithParticipants(t *testing.T){
+	defer mongodb.Meetings.Collection.Drop(mongodb.Meetings.Context)
+
+	var member = primitive.NewObjectID()
+	var participants = models.MeetingParticipants{
+		Members: append(make([]primitive.ObjectID, 0), member),
+	}
+
+	Meeting1Data.Participants = &participants
+
+	var meeting models.Meeting
+
+	b, errMarshal := json.Marshal(Meeting1Data)
+	assert.NilError(t, errMarshal)
+
+	res, err := executeRequest("POST", "/meetings", bytes.NewBuffer(b))
+	assert.NilError(t, err)
+	assert.Equal(t, res.Code, http.StatusOK)
+
+	Meeting1, err = mongodb.Meetings.GetMeeting(meeting.ID)
+	assert.NilError(t, err)
+
+	json.NewDecoder(res.Body).Decode(&meeting)
+	assert.Equal(t, meeting.ID, Meeting1.ID)
+	assert.Equal(t, meeting.Place, Meeting1.Place)
+	assert.Equal(t, len(meeting.Participants.Members), 1)
+	assert.Equal(t, meeting.Participants.Members[0], member)	
 }
 
 func TestGetMeeting(t *testing.T){
@@ -58,7 +89,24 @@ func TestGetMeeting(t *testing.T){
 	Meeting1, err := mongodb.Meetings.CreateMeeting(Meeting1Data)
 	assert.NilError(t, err)
 
+	var meeting models.Meeting
+
 	res, err := executeRequest("GET", "/meetings/"+Meeting1.ID.Hex(), nil)
 	assert.NilError(t, err)
 	assert.Equal(t, res.Code, http.StatusOK)
+
+	json.NewDecoder(res.Body).Decode(&meeting)
+
+	assert.Equal(t, meeting.ID, Meeting1.ID)
+}
+
+func TestGetMeetingWrongID(t *testing.T){
+	defer mongodb.Meetings.Collection.Drop(mongodb.Meetings.Context)
+
+	_, err := mongodb.Meetings.CreateMeeting(Meeting1Data)
+	assert.NilError(t, err)
+
+	res, err := executeRequest("GET", "/meetings/wrong", nil)
+	assert.NilError(t, err)
+	assert.Equal(t, res.Code, http.StatusNotFound)
 }
