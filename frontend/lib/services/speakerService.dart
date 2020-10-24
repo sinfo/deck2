9b72de6e8de312773279c7e2decf7b380291fb7a
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2020-10-24 16:57:57 +0100

    Implemented method for /speakers endpoint.

--- frontend/lib/services/speakerService.dart
+++ frontend/lib/services/speakerService.dart
@@ -1,3 +1,23 @@
+import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'package:frontend/models/speaker.dart';
 import 'package:frontend/services/service.dart';
 
-class SpeakerService extends Service {}
+class SpeakerService extends Service {
+  Future<List<Speaker>> getSpeakers({String name, int eventId}) async {
+    var queryParameters = {'name': name, 'event': eventId};
+
+    Response<String> response =
+        await dio.get("/speakers", queryParameters: queryParameters);
+
+    if (response.statusCode == 200) {
+      final responseJson = json.decode(response.data) as List;
+      List<Speaker> speakers =
+          responseJson.map((e) => Speaker.fromJson(e)).toList();
+      return speakers;
+    } else {
+      print("error");
+      return [];
+    }
+  }
+}
