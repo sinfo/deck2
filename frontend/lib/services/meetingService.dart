476121fafa617e6a6219be0ae80c50fdc699e07b
Rodrigo Silva <rodrigo.silva96@outlook.pt>
2021-07-13 23:55:04 +0100

    Added better exception handling

--- frontend/lib/services/meetingService.dart
+++ frontend/lib/services/meetingService.dart
@@ -2,93 +2,107 @@ import 'dart:convert';
 import 'package:frontend/models/meeting.dart';
 import 'package:frontend/services/service.dart';
 import 'package:dio/dio.dart';
+import 'package:frontend/components/deckException.dart';
+import 'dart:io';
 
 class MeetingService extends Service {
+  Future<List<Meeting>> getMeetings(
+      {team: String, company: String, event: int}) async {
+    var queryParameters = {'team': team, 'company': company, 'event': event};
 
-  Future<List<Meeting>> getMeetings({team : String, company : String, event : int}) async {
-      var queryParameters = {
-        'team' : team,
-        'company' : company,
-        'event' : event
-      };
+    Response<String> response =
+        await dio.get("/meetings", queryParameters: queryParameters);
 
-      Response<String> response = 
-          await dio.get("/meetings", queryParameters : queryParameters);
-
-      if(response.statusCode == 200) {
-          final responseJson = json.decode(response.data) as List;
-          List<Meeting> meetings = 
-              responseJson.map( (e) => Meeting.fromJson(e)).toList();
-          return meetings;
-      } else {
-          // TODO : Handle Error
-          print("error");
-          return [];
-      }
+    try {
+      final responseJson = json.decode(response.data) as List;
+      List<Meeting> meetings =
+          responseJson.map((e) => Meeting.fromJson(e)).toList();
+      return meetings;
+    } on SocketException {
+      throw DeckException('No Internet connection');
+    } on HttpException {
+      throw DeckException('Not found');
+    } on FormatException {
+      throw DeckException('Wrong format');
+    }
   }
 
-   Future<Meeting> createMeeting(DateTime begin, DateTime end, String place, MeetingParticipants participants) async {
-      var body = {
-         "begin" : begin,
-         "end" : end,
-         "place" : place,
-         "participants" : participants
-      };
+  Future<Meeting> createMeeting(DateTime begin, DateTime end, String place,
+      MeetingParticipants participants) async {
+    var body = {
+      "begin": begin,
+      "end": end,
+      "place": place,
+      "participants": participants
+    };
 
-      Response<String> response = await dio.post("/meetings", data: body );
+    Response<String> response = await dio.post("/meetings", data: body);
 
-      if (response.statusCode == 200){
-        return Meeting.fromJson(json.decode(response.data));
-      } else {
-        // TODO: Handle Error
-        print("error");
-        return null;
-      }
-
-   }
-
-   Future<Meeting> getMeeting(String id) async {
-      Response<String> response = await dio.get("/meetings/" + id);
-
-      if ( response.statusCode == 200) {
-        return Meeting.fromJson(json.decode(response.data));
-      }
+    try {
+      return Meeting.fromJson(json.decode(response.data));
+    } on SocketException {
+      throw DeckException('No Internet connection');
+    } on HttpException {
+      throw DeckException('Not found');
+    } on FormatException {
+      throw DeckException('Wrong format');
     }
+  }
 
-   Future<Meeting> deleteMeeting(String id) async {
-      Response<String> response = await dio.delete("/meetings/" + id);
-      if (response.statusCode == 200) {
-        return Meeting.fromJson(json.decode(response.data));
-      } else {
-        // TODO: Handle Error
-        print("error");
-        return null;
-      }
-   }
+  Future<Meeting> getMeeting(String id) async {
+    Response<String> response = await dio.get("/meetings/" + id);
 
-   Future<Meeting> updateMeeting(String id, DateTime begin, DateTime end, String place,
-   //, MeetingParticipants participants
-   ) async {
-      var body = {
-         "begin" : begin,
-         "end" : end,
-         "place" : place
-         //, "participants" : participants
-      };
+    try {
+      return Meeting.fromJson(json.decode(response.data));
+    } on SocketException {
+      throw DeckException('No Internet connection');
+    } on HttpException {
+      throw DeckException('Not found');
+    } on FormatException {
+      throw DeckException('Wrong format');
+    }
+  }
+
+  Future<Meeting> deleteMeeting(String id) async {
+    Response<String> response = await dio.delete("/meetings/" + id);
+    try {
+      return Meeting.fromJson(json.decode(response.data));
+    } on SocketException {
+      throw DeckException('No Internet connection');
+    } on HttpException {
+      throw DeckException('Not found');
+    } on FormatException {
+      throw DeckException('Wrong format');
+    }
+  }
+
+  Future<Meeting> updateMeeting(
+    String id,
+    DateTime begin,
+    DateTime end,
+    String place,
+    //, MeetingParticipants participants
+  ) async {
+    var body = {
+      "begin": begin,
+      "end": end,
+      "place": place
+      //, "participants" : participants
+    };
 
     Response<String> response = await dio.put("/meetings/" + id, data: body);
-    if (response.statusCode == 200) {
-        return Meeting.fromJson(json.decode(response.data));
-      } else {
-        // TODO: Handle Error
-        print("error");
-        return null;
-      }
-   }
-
-    Future<Meeting> uploadMeetingMinute(String id, String minute) async {
-        // TODO: Implement : https://pub.dev/packages/dio
+    try {
+      return Meeting.fromJson(json.decode(response.data));
+    } on SocketException {
+      throw DeckException('No Internet connection');
+    } on HttpException {
+      throw DeckException('Not found');
+    } on FormatException {
+      throw DeckException('Wrong format');
     }
+  }
 
-
+  Future<Meeting> uploadMeetingMinute(String id, String minute) async {
+    // TODO: Implement : https://pub.dev/packages/dio
+  }
 }
