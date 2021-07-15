1f520801b63864163e85c733b4d524abeb1104c0
Larissa-Tomaz <larissa.tomaz@tecnico.ulisboa.pt>
2021-07-15 17:16:05 +0100

    Session.service - work in progress

---
+++ frontend/lib/services/sessionService.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:frontend/models/session.dart';
+import 'package:frontend/services/service.dart';
+import 'package:frontend/components/deckException.dart';
+
+class SessionService extends Service {
+  Future<List<Session>> getSessions(
+      {int? event, String? company, String? kind}) async {
+    var queryParameters = {'event': event, 'company': company, 'kind': kind};
+
+    Response<String> response =
+        await dio.get("/sessions", queryParameters: queryParameters);
+
+    try {
+      final responseJson = json.decode(response.data!) as List;
+      List<Session> sessions =
+          responseJson.map((e) => Session.fromJson(e)).toList();
+      return sessions;
+    } on SocketException {
+      throw DeckException('No Internet connection');
+    } on HttpException {
+      throw DeckException('Not found');
+    } on FormatException {
+      throw DeckException('Wrong format');
+    }
+  }
+
+  Future<Session> getSession(String id) async {
+    Response<String> response = await dio.get('/sessions/$id');
+
+    try {
+      return Session.fromJson(json.decode(response.data!));
+    } on SocketException {
+      throw DeckException('No Internet connection');
+    } on HttpException {
+      throw DeckException('Not found');
+    } on FormatException {
+      throw DeckException('Wrong format');
+    }
+  }
+
+  /* TODO -- updateSession */
+
+}
