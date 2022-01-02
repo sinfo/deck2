52e4eed94fe9648de0498177d157a4290142d5e6
Sofia-Morgado <sofia.morgado@tecnico.ulisboa.pt>
2022-01-02 13:39:48 +0000

    started to implement team and member notifier - can change team name

--- frontend/lib/components/appbar.dart
+++ frontend/lib/components/appbar.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/components/deckTheme.dart';
 import 'package:frontend/components/eventNotifier.dart';
 import 'package:frontend/main.dart';
 import 'package:frontend/models/event.dart';
+import 'package:frontend/routes/company/CompanyScreen.dart';
+import 'package:frontend/routes/member/MemberScreen.dart';
+import 'package:frontend/routes/speaker/SpeakerScreen.dart';
 import 'package:frontend/services/eventService.dart';
 import 'package:provider/provider.dart';
+import 'package:frontend/models/company.dart';
+import 'package:frontend/models/member.dart';
+import 'package:frontend/models/speaker.dart';
+import 'package:frontend/services/companyService.dart';
+import 'package:frontend/services/memberService.dart';
+import 'package:frontend/services/speakerService.dart';
+
+enum SortingMethod {
+  RANDOM,
+  NUM_PARTICIPATIONS,
+  LAST_PARTICIPATION,
+}
 
 class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
   final bool disableEventChange;
   final List<Widget>? actions;
+
+  @override
+  final Size preferredSize;
+
   CustomAppBar({
     Key? key,
     required this.disableEventChange,
@@ -16,18 +36,24 @@ class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
         super(key: key);
 
   @override
-  final Size preferredSize;
-
-  @override
   _CustomAppBarState createState() =>
       _CustomAppBarState(disableEventChange, actions);
 }
 
 class _CustomAppBarState extends State<CustomAppBar> {
-  EventService _eventService = EventService();
+  late Future<List<Company>> companies;
+  late Future<List<Speaker>> speakers;
+  late Future<List<Member>> members;
   late Future<List<int>> _eventIds;
   final bool disableEventChange;
   final List<Widget>? actions;
+
+  EventService _eventService = EventService();
+  CompanyService companyService = new CompanyService();
+  SpeakerService speakerService = new SpeakerService();
+  MemberService memberService = new MemberService();
+
+  final _searchController = TextEditingController();
 
   _CustomAppBarState(this.disableEventChange, this.actions);
 
@@ -43,26 +69,25 @@ class _CustomAppBarState extends State<CustomAppBar> {
   Widget build(BuildContext context) {
     EventNotifier notifier = Provider.of<EventNotifier>(context);
     int current = notifier.event.id;
-    return AppBar(
-      actions: actions,
-      title: Row(
-        children: [
+    return Column(children: [
+      AppBar(
+        actions: actions,
+        title: Row(children: [
           InkWell(
             child: SizedBox(
               height: kToolbarHeight,
               child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+                padding: const EdgeInsets.fromLTRB(0, 4, 4, 4),
                 child: Image.asset(
-                  'assets/logo.png',
-                  color: Colors.grey[400],
-                  fit: BoxFit.cover,
+                  'assets/logo_deck.png',
+                  fit: BoxFit.fill,
                 ),
               ),
             ),
           ),
           if (!disableEventChange)
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.fromLTRB(4.0, 8.0, 24.0, 8.0),
               child: FutureBuilder(
                 future: _eventIds,
                 builder: (context, snapshot) {
@@ -103,9 +128,181 @@ class _CustomAppBarState extends State<CustomAppBar> {
                   }
                 },
               ),
-            )
-        ],
+            ),
+          Expanded(
+              child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    filled: true,
+                    fillColor: Provider.of<ThemeNotifier>(context).isDark
+                        ? Colors.grey[800]
+                        : Colors.white,
+                    hintText: 'Search Company, Speaker or Member',
+                    prefixIcon: Icon(Icons.search),
+                    suffixIcon: _searchController.text.length != 0
+                        ? IconButton(
+                            onPressed: () {
+                              _searchController.clear();
+                              setState(() {});
+                            },
+                            icon: Icon(Icons.clear),
+                          )
+                        : null,
+                  ),
+                  onChanged: (newQuery) {
+                    setState(() {});
+                    if (_searchController.text.length > 1) {
+                      this.companies = companyService.getCompanies(
+                          name: _searchController.text);
+                      this.speakers = speakerService.getSpeakers(
+                          name: _searchController.text);
+                      this.members = memberService.getMembers(
+                          name: _searchController.text);
+                    }
+                  })),
+        ]),
       ),
-    );
+      ...getResults(MediaQuery.of(context).size.height / 2)
+    ]);
+  }
+
+  List<Widget> getResults(double height) {
+    if (_searchController.text.length > 1) {
+      return [
+        Container(
+            decoration: new BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: FutureBuilder(
+                future:
+                    Future.wait([this.speakers, this.companies, this.members]),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List<dynamic> data = snapshot.data as List<dynamic>;
+
+                    List<Speaker> speaksMatched = data[0] as List<Speaker>;
+                    List<Company> compsMatched = data[1] as List<Company>;
+                    List<Member> membsMatched = data[2] as List<Member>;
+                    return searchResults(
+                        speaksMatched, compsMatched, membsMatched, height);
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                }))
+      ];
+    } else {
+      return [];
+    }
+  }
+
+  Widget searchResults(List<Speaker> speakers, List<Company> companies,
+      List<Member> members, double listHeight) {
+    List<Widget> results = getListCards(speakers, companies, members);
+    return Container(
+        constraints: BoxConstraints(maxHeight: listHeight),
+        child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: results.length,
+            itemBuilder: (BuildContext context, int index) {
+              return results[index];
+            }));
+  }
+
+  List<Widget> getListCards(
+      List<Speaker> speakers, List<Company> companies, List<Member> members) {
+    List<Widget> results = [];
+    if (speakers.length != 0) {
+      results.add(getDivider("Speakers"));
+      results.addAll(speakers.map(
+          (e) => SearchResultWidget(speaker: e, index: speakers.indexOf(e))));
+    }
+    if (companies.length != 0) {
+      results.add(getDivider("Companies"));
+      results.addAll(companies.map(
+          (e) => SearchResultWidget(company: e, index: companies.indexOf(e))));
+    }
+    if (members.length != 0) {
+      results.add(getDivider("Members"));
+      results.addAll(members.map(
+          (e) => SearchResultWidget(member: e, index: members.indexOf(e))));
+    }
+    return results;
+  }
+
+  Widget getDivider(String name) {
+    return Card(
+        margin: EdgeInsets.zero,
+        child: Column(
+          children: [
+            Container(
+              child: Text(name, style: TextStyle(fontSize: 18)),
+              margin: EdgeInsets.fromLTRB(0, 8, 0, 4),
+            ),
+          ],
+        ));
+  }
+}
+
+class SearchResultWidget extends StatelessWidget {
+  final Company? company;
+  final Speaker? speaker;
+  final Member? member;
+  final int? index;
+  const SearchResultWidget(
+      {Key? key, this.company, this.speaker, this.member, this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            if (company != null) {
+              return CompanyScreen(company: company!);
+            } else if (speaker != null) {
+              return SpeakerScreen(speaker: speaker!);
+            }
+            return MemberScreen(member: member!);
+          }));
+        },
+        child: Center(
+          child: ListTile(
+            leading: CircleAvatar(
+              foregroundImage: NetworkImage(getImageURL()),
+              backgroundImage: AssetImage(
+                'assets/noImage.png',
+              ),
+            ),
+            title: Text(getName()),
+          ),
+        ));
+  }
+
+  String getImageURL() {
+    if (this.company != null) {
+      return this.company!.companyImages.internal;
+    } else if (this.speaker != null) {
+      return this.speaker!.imgs!.internal!;
+    } else if (this.member != null) {
+      return this.member!.image!;
+    } else {
+      //ERROR case
+      return "";
+    }
+  }
+
+  String getName() {
+    if (this.company != null) {
+      return this.company!.name;
+    } else if (this.speaker != null) {
+      return this.speaker!.name;
+    } else if (this.member != null) {
+      return this.member!.name!;
+    } else {
+      //ERROR case
+      return "";
+    }
   }
 }
