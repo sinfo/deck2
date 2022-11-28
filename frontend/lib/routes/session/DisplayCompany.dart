152a0da6d1a22ae7ec84aa04b39033f276f6691f
CatarinaBeirolas <catarinabeirolasvilhena@hotmail.com>
2022-11-28 22:29:44 +0000

    Added CircularProgressIndicator to loading companies tab

--- frontend/lib/routes/session/DisplayCompany.dart
+++ frontend/lib/routes/session/DisplayCompany.dart
@@ -40,49 +40,66 @@ class _DisplayCompanyState extends State<DisplayCompany> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(186, 196, 242, 0.1),
-      body: new ListView(
-        padding: EdgeInsets.symmetric(horizontal: 32),
-        physics: BouncingScrollPhysics(),
-        children: [
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-            padding: EdgeInsets.fromLTRB(17, 15, 17, 15),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 7.0,
-                      color: Colors.grey.withOpacity(0.3),
-                      offset: new Offset(0, 3),
-                      spreadRadius: 4.0),
-                ]),
-            child: ListTile(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => CompanyScreen(
-                              company: company!,
-                            )));
-              },
-              title: Text(companyName,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 18, color: Colors.black)),
-              subtitle: Text(companySite!),
-              leading: CircleAvatar(
-                radius: 26.0,
-                foregroundImage: NetworkImage(
-                  companyImage!.internal,
+    if (companyName != "not filled yet") {
+      return Scaffold(
+        backgroundColor: Color.fromRGBO(186, 196, 242, 0.1),
+        body: new ListView(
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          physics: BouncingScrollPhysics(),
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              padding: EdgeInsets.fromLTRB(17, 15, 17, 15),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 7.0,
+                        color: Colors.grey.withOpacity(0.3),
+                        offset: new Offset(0, 3),
+                        spreadRadius: 4.0),
+                  ]),
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CompanyScreen(
+                                company: company!,
+                              )));
+                },
+                title: Text(companyName,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 18, color: Colors.black)),
+                subtitle: Text(companySite!),
+                leading: CircleAvatar(
+                  radius: 26.0,
+                  foregroundImage: NetworkImage(
+                    companyImage!.internal,
+                  ),
+                  backgroundImage: AssetImage('assets/noImage.png'),
                 ),
-                backgroundImage: AssetImage('assets/noImage.png'),
               ),
+            )
+          ],
+        ),
+      );
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Container(
+              height: 40,
+              width: 40,
+              margin: EdgeInsets.all(5),
+              child: CircularProgressIndicator(),
             ),
-          )
+          ),
         ],
-      ),
-    );
+      );
+    }
   }
 }
