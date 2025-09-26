f5da0beebcc87a2d29ff7d998bdac122909b7e02
Lucas Pinto <github@lucks.pt>
2025-09-26 17:22:01 +0100

    chore: add debug for staging

--- frontend/src/composables/useBulkEmails.ts
+++ frontend/src/composables/useBulkEmails.ts
@@ -181,6 +181,9 @@ const speakerEmailFetcher: EmailFetcher<SpeakerWithParticipation> = {
     try {
       const response = await getSpeakerById(speaker.id);
       const speakerData = response.data;
+
+      console.log("speakerEmailFetcher", speakerData);
+
       if (
         !speakerData.contactObject?.mails ||
         speakerData.contactObject.mails.length === 0
@@ -253,6 +256,7 @@ export const useBulkEmails = <T extends BulkEmailEntity>(
 
         try {
           email = await emailFetcher.getEmail(entity);
+          console.log('useBulkEmails', email, entity)
           valid = processEmail(email, entity);
         } catch (error) {
           valid = {
